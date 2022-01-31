de603917479edd5f946eb2e8caf39c3f6f491e3a
BarnaTB <barnabastb2@gmail.com>
2022-02-01 01:40:58 +0300

    Setup app routing and project structure

--- main/app.py
+++ main/app.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
 from utils.core import get_env_variable
-
+from main.settings import logger, BaseSettings, Settings
 
 app = FastAPI(debug=get_env_variable("DEBUG", 0))
+
+@app.on_event("startup")
+def get_settings() -> BaseSettings:
+    logger.info("Loading config settings from the environment...")
+    return Settings()
--- main/settings.py
+++ main/settings.py
@@ -3,14 +3,13 @@ import logging
 from pydantic import BaseSettings
 
 from utils.core import get_env_variable
-from main.app import app
 
 
 logger = logging.getLogger("uvicorn")
 
 
 class Settings(BaseSettings):
-    debug: bool = get_env_variable("DEBUG", 0, required=True)
+    debug: bool = get_env_variable("DEBUG", 1, required=True)
     news_api_urls: str = get_env_variable("NEWS_API_URLS", required=True)
 
 
